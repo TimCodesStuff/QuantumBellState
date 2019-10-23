a92282667dbd404aa34721a271f7bb128bf7ce24
Tim <TimCodesStuff@hotmail.com>
2019-10-23 12:52:01 -0700

    Adding CNOT gate to entangle two qubits.

--- Bell/Driver.cs
+++ Bell/Driver.cs
@@ -11,14 +11,12 @@ namespace Bell
         {
             using (var qsim = new QuantumSimulator())
             {
-                // Try initial values
                 Result[] initials = new Result[] { Result.Zero, Result.One };
                 foreach (Result initial in initials)
                 {
                     var res = BellTest.Run(qsim, 1000, initial).Result;
-                    var (numZeros, numOnes) = res;
-                    System.Console.WriteLine(
-                        $"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4}");
+                    var (numZeros, numOnes, agree) = res;
+                    Console.WriteLine($"Init:{initial,-4} 0s={numZeros,-4} 1s={numOnes,-4} agree={agree,-4}");
                 }
             }
 
